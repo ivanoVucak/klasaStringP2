34cbc9616f6f34a0c637ece3985824b3793b5fc5
ivanoVucak <145533812+ivanoVucak@users.noreply.github.com>
2023-10-25 08:51:08 +0200

    Add files via upload

---
+++ klasaStringP2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace klasaStringP2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string tekst;
+            Console.WriteLine("Unesi neku recenicu: ");
+            tekst = Convert.ToString(Console.ReadLine());
+
+           
+     
+        Console.WriteLine(tekst.Replace((" "), "_"));
+
+        Console.ReadKey();
+            
+        }
+    }
+}
